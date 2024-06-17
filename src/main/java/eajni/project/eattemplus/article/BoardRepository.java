e4e0478ff5689e4718e3222b089146a736fc4ed6
eajni <eajni.h@gmail.com>
2024-06-17 10:04:59 +0900

    Revert "0616"
    This reverts commit bc6742be4b09ac950f4cc576d573270a7bcc3fdd.

--- src/main/java/eajni/project/eattemplus/article/BoardRepository.java
+++
@@ -1,6 +0,0 @@
-package eajni.project.eattemplus.article;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Article, Long> {
-}
